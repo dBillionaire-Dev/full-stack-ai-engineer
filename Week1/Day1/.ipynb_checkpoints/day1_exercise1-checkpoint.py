3f70a44e4f6a515d571d56db4948e2bc4e2aa4b1
dBillionaireDev <nezerekunke@gmail.com>
2026-06-07 23:46:58 +0100

    feat: starting out the ml journey

---
+++ Week1/Day1/.ipynb_checkpoints/day1_exercise1-checkpoint.py
@@ -0,0 +1,7 @@
+print("Hello, AI World!")
+count = 5
+while count > 0:
+    print("Count: ", count)
+    count -= 1
+
+print("Loop finished!")
---
+++ Week1/Day1/day1_exercise1.py
@@ -0,0 +1,8 @@
+print("Hello, AI World!")
+
+count = 0
+while count < 6:
+    print("Count: ", count)
+    count += 1
+
+print("Loop finished!")
