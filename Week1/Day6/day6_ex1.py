0bed8f8bf4eebb12ca3b3ec5162f913a8e9a6473
dBillionaireDev <nezerekunke@gmail.com>
2026-06-09 15:28:29 +0100

    feat: progress on file reading in python 'r|w|a|x

--- Week1/Day6/day6_ex1.py
+++ Week1/Day6/day6_ex1.py
@@ -4,10 +4,32 @@ def count_words_and_lines(filename):
             lines = file.readlines()
             line_count = len(lines)
             word_count = sum(len(line.split()) for line in lines)
-            
-            print(f"Number of lines: {line_count}")
-            print(f"Number of words: {word_count}")
+
+            print(f"Number of lines in {filename}: {line_count}")
+            print(f"Number of words in {filename}: {word_count}")
     except FileNotFoundError:
         print(f"File {filename} not found!")
-        
+
+
 count_words_and_lines("sample.txt")
+count_words_and_lines("fruits.txt")
+
+
+with open("sample.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+with open("fruits.txt", "a") as fruits:
+    fruits.write("Grapes")
+
+with open("fruits.txt", "r") as fruits:
+    new_content = fruits.readlines()
+    for line in new_content:
+        print(line.strip())
+
+with open("new_file.txt", "a") as file:
+    file.writelines(["Hello World!", "I am an AI Engineer", "A fullstack developer"])
+
+with open("new_file.txt", "r") as file:
+    content = file.read()
+    print(content)
